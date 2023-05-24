a8910e7aecea377b6c5d2f2af8c2ab252e91f7e4
朝倉滉人 <asakurahiroto@ASAKURA.local>
2023-05-25 05:01:34 +0900

    ファイル６の追加

--- lecture06/Remover.java
+++ lecture06/Remover.java
@@ -3,11 +3,19 @@ package lecture06;
 import java.io.File;
 
 public class Remover {
+  /**
+   * run
+   * @param args
+   */
   void run(String[] args){
     File directory = new File(args[0]);
     removeDirectory(directory);
   }
 
+  /**
+   * ディレクトリー削除を行うメソッド
+   * @param dir
+   */
   void removeDirectory(File dir){
     if (!dir.exists()){
       return;
@@ -21,6 +29,10 @@ public class Remover {
     dir.delete();
   }
 
+  /**
+   * main
+   * @param args
+   */
   public static void main(String[] args){
     Remover app = new Remover();
     app.run(args);

9af3f4692bd8478a042654ec9f5fa85158b4ea0c
朝倉滉人 <asakurahiroto@ASAKURA.local>
2023-06-08 17:19:16 +0900

    ファイル8の追加

--- lecture08/CaesarCipher.java
+++ lecture08/CaesarCipher.java
@@ -3,6 +3,11 @@ package lecture08;
 import java.io.*;
 
 public class CaesarCipher {
+  /**
+   * run
+   * @param args
+   * @throws IOException
+   */
   void run(String[] args) throws IOException {
     InputStream in =  new FileInputStream(args[1]);
     OutputStream out = new FileOutputStream(args[2]);
@@ -11,6 +16,13 @@ public class CaesarCipher {
     out.close();
   }
 
+  /**
+   * カエサルメソッド
+   * @param keyNum
+   * @param in
+   * @param printer
+   * @throws IOException
+   */
   void caesar(Integer keyNum, InputStream in, OutputStream printer) throws IOException {
     Integer word;
     while ((word = in.read()) != -1) {
@@ -25,6 +37,11 @@ public class CaesarCipher {
 
   }
 
+  /**
+   * main
+   * @param args
+   * @throws IOException
+   */
   public static void main(String[] args) throws IOException {
     CaesarCipher app = new CaesarCipher();
     app.run(args);
